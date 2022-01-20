5348e194d5fea398b3bbc5aaec984b614fdc3ed6
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-20 02:35:42 +0000

    implement function and test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
 
-module.exports = wordSearch
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  let newArray = [];
+  for (i = 0; i < letters.length; i++) {
+    for (j = 0; j < letters[i].length; j++) {
+      if (letters[i][j] === undefined) continue;
+      if (newArray[j] === undefined) newArray[j] = [];
+
+      newArray[j][i] = letters[i][j];
+    }
+  }
+
+  const verticalJoin = newArray.map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
